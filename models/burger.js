0c381730ef3349bf5634e32fa55bfed433606194
Jack Walker <jackw2050@gmail.com>
2016-08-22 19:49:27 -0500

    small update

--- models/burger.js
+++ models/burger.js
@@ -1,31 +1,58 @@
-/*
-Here is where you setup a model for how to interface with the database.
-*/
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define('Burger', {
+    burger_name: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN,
+    date: DataTypes.DATE
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Burger;
+};
 
-var orm = require('../config/orm.js');
+
+
+
+
 
 var burger = {
-	all: function(cb) {
-		orm.all('burgers', function(res){
-			cb(res);
-		});
-	},
-	//cols and vals are arrays
-	create: function(cols, vals, cb) {
-		orm.create('burgers', cols, vals, function(res){
-			cb(res);
-		});
-	},
-	update: function(objColVals, condition, cb) {
-		orm.update('burgers', objColVals, condition, function(res){
-			cb(res);
-		});
-	},
-	delete: function(condition, cb){
-		orm.delete('burgers', condition, function(res){
-			cb(res);
-		});
-	}
+    all: function(cb) {
+              User.findAll().then(function(burgerData) {
+            console.log("users " + burgerData[0].burger_name);
+            console.log(JSON.stringify(burgerData, null, 2));
+            cb(res);
+        })
+     /*   orm.all('burgers', function(res) {
+            cb(res);
+
+        });
+
+*/
+
+
+
+
+    },
+    //cols and vals are arrays
+    create: function(cols, vals, cb) {
+        orm.create('burgers', cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update('burgers', objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete('burgers', condition, function(res) {
+            cb(res);
+        });
+    }
 };
 
 module.exports = burger;
--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-/*
-Here is where you set up your server file.
-express middleware.
-*/
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')

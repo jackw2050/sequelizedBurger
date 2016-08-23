b8c86d21472325626aa23cb878fa3b7406a5f54a
Jack Walker <jackw2050@gmail.com>
2016-08-23 18:21:21 -0500

    fixed findAll

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,26 +3,40 @@ Here is where you create all the functions that will do the routing for your app
 */
 var express = require('express');
 var router = express.Router();
-var burger = require('../models/burger.js');
+//var burger = require('../models/burger.js');
+var models  = require('../models');
 
 router.get('/', function(req,res) {
 	res.redirect('/burgers')
 });
 
 router.get('/burgers', function(req,res) {
-	burger.all(function(data){
-		var hbsObject = {burgers : data}
-	
+	models.Burger.findAll(
+		{//include: [ models.Burger ]
+		}).then(function(burgerData) {
+		var hbsObject = {burgers : burgerData}
+	console.log(JSON.stringify(burgerData, null, 2));
 		res.render('index', hbsObject);
 	});
 });
 
+
+
 router.post('/burgers/create', function(req,res) {
-	//console.log(req.body);
-	burger.create(['burger_name', 'devoured'], [req.body.name, req.body.devoured], function(data){
-		res.redirect('/burgers')
-	});
+
+  models.Burger.create({
+    burger_name: req.body.burger_name,
+    devoured: req.params.devoured
+  })
+  .then(function() {
+    res.redirect('/');
+  });
+
 });
+
+
+
+
 
 router.put('/burgers/update/:id', function(req,res) {
 	var condition = 'id = ' + req.params.id;
--- models/burger.js
+++ models/burger.js
@@ -4,12 +4,6 @@ module.exports = function(sequelize, DataTypes) {
     burger_name: DataTypes.STRING,
     devoured: DataTypes.BOOLEAN,
     date: DataTypes.DATE
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
   });
   return Burger;
 };
@@ -18,41 +12,3 @@ module.exports = function(sequelize, DataTypes) {
 
 
 
-
-var burger = {
-    all: function(cb) {
-              User.findAll().then(function(burgerData) {
-            console.log("users " + burgerData[0].burger_name);
-            console.log(JSON.stringify(burgerData, null, 2));
-            cb(res);
-        })
-     /*   orm.all('burgers', function(res) {
-            cb(res);
-
-        });
-
-*/
-
-
-
-
-    },
-    //cols and vals are arrays
-    create: function(cols, vals, cb) {
-        orm.create('burgers', cols, vals, function(res) {
-            cb(res);
-        });
-    },
-    update: function(objColVals, condition, cb) {
-        orm.update('burgers', objColVals, condition, function(res) {
-            cb(res);
-        });
-    },
-    delete: function(condition, cb) {
-        orm.delete('burgers', condition, function(res) {
-            cb(res);
-        });
-    }
-};
-
-module.exports = burger;
